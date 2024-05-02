03adc1d755c7a90a9b2a4ef6bfb99c49183005d0
Danilo Teixeira <danilo.rt@outlook.com>
2024-05-02 00:23:35 -0300

    feat: add movie trailer

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,10 +3,12 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'prime-movies'`, () => {
+  it(`should have as title 'api-movies'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('prime-movies');
+    expect(app.title).toEqual('api-movies');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('prime-movies app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'api-movies app is running!'
+    );
   });
 });
--- src/app/core/services/movie/movie.service.ts
+++ src/app/core/services/movie/movie.service.ts
@@ -3,6 +3,7 @@ import { Injectable, inject } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { API_INFO } from '../../data/api-info';
 import { ApiResult, Movie } from '../../models/Movie.interface';
+import { MovieVideo, VideoResult } from '../../models/MovieVideo.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -10,13 +11,19 @@ import { ApiResult, Movie } from '../../models/Movie.interface';
 export class MovieService {
   private readonly http = inject(HttpClient);
   private readonly baseUrl = API_INFO.BASE_URL;
-  public getTopRatedMovies(): Observable<ApiResult[]> {
+  getTopRatedMovies(): Observable<ApiResult[]> {
     const url = `${this.baseUrl}/top_rated`;
 
     let params = new HttpParams().append('page', '1');
 
     return this.http
       .get<Movie>(url, { params })
-      .pipe(map((res) => res.results.slice(0, 10)));
+      .pipe(map((res) => res?.results.slice(0, 10)));
+  }
+
+  getVideoInfoById(movieId: number): Observable<VideoResult[]> {
+    const url = `${this.baseUrl}/${movieId}/videos`;
+
+    return this.http.get<MovieVideo>(url).pipe(map((res) => res?.results));
   }
 }
